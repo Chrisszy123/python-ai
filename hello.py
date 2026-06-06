546b2c0db1b375460c87f612cab032deab10447d
chrisszy123 <bassygoodluck@gmail.com>
2026-06-06 01:05:36 +0100

    Refactor code structure for improved readability and maintainability

--- hello.py
+++ hello.py
@@ -1,4 +1,17 @@
-import requests
+def greet():
+    print("Hello, World!")
+    pass #when you dont want to return anything you can use pass to avoid an error
 
-response = requests.get("https://www.google.com")
-print(response.status_code)
+greet()
+
+def check_weather(temp):
+    if temp > 30:
+        print("It's hot outside")
+    elif temp < 10:
+        print("It's cold outside")
+    else:
+        print("The weather is nice")
+
+check_weather(35)  # It's hot outside
+check_weather(5)   # It's cold outside
+check_weather(20)  # The weather is nice
